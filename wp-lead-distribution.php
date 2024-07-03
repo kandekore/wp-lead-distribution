408ddd133cdbe690bf1ed8b74b32afb8ab532ab1
Kandekore <darren@kandekore.net>
2024-07-03 12:05:56 +0100

    version

--- wp-lead-distribution.php
+++ wp-lead-distribution.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WordPress Lead Distribution
  * Description: Collects and distributes leads to users on a subscription basis.
- * Version: 1.0.3
+ * Version: 1.0.5
  * Author: D.Kandekore
  */
 
